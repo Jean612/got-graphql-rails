305413545ea43279ab81b1866872bfd7543d943a
Jean Pier <diegojeanpier.dc.@gmail.com>
2023-02-20 02:53:59 -0500

    Add queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,34 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :characters, [CharacterType], null: false, description: "List all characters"
+    def characters
+      Character.all
     end
+    
+    field :character_count, Integer, null: false
+    def character_count
+      Character.count
+    end
+    
+    field :character_by_name, CharacterType, "Find Character by name" do
+      argument :name, String
+    end
+    def character_by_name(name:)
+      Character.find_by_name(name)
+    end
+    
+    field :character, CharacterType, "Find Character by ID" do
+      argument :id, ID
+    end
+    def character(id:)
+      Character.find(id)
+    end
+    
+    field :locations, [LocationType], null: false, description: "List all locations"
+    def locations
+      Location.all
+    end
+    
   end
 end
