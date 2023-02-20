4a60e0ad2dc4405a71f0cad14dafbc311d783ebb
Jean Pier <diegojeanpier.dc.@gmail.com>
2023-02-20 02:54:15 -0500

    Add validations in character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,7 @@
 class Character < ApplicationRecord
-  belongs_to :location, :class_name => 'Location'
-  belongs_to :origin, :class_name => 'Location'
+  belongs_to :location, :class_name => 'Location', optional: true
+  belongs_to :origin, :class_name => 'Location', optional: true
+  
+  validates :name, presence: true, length: { minimum: 5 }
+  validates_uniqueness_of :name, on: [:create, :update], message: "El nombre del personaje ya existe"
 end
