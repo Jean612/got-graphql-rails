b15845735ffdebb7da1209062ffe34166b9e47fb
Jean Pier <diegojeanpier.dc.@gmail.com>
2023-02-20 02:53:44 -0500

    Add mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,43 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+    
+    field :create_character, mutation: Mutations::CreateCharacter
+    field :update_character, mutation: Mutations::UpdateCharacter
+    field :destroy_character, mutation: Mutations::DestroyCharacter
+       
+    field :add_character, CharacterType, null: false, description: "Add a character" do
+      argument :name, String
+      argument :status, String, required: false
+      argument :species, String, required: false
+      argument :location_id, Integer, required: false
+      argument :origin_id, Integer, required: false
+    end
+    def add_character(args)
+      Character.create(args)
+    end
+    
+    field :delete_character, [CharacterType], null: false, description: "remove character" do
+      argument :id, ID, required: true
+    end
+    def delete_character(id:)
+      Character.destroy(id)
+      Character.all
+    end
+    
+    field :edit_character, CharacterType, null:false, description: "edit character" do
+      argument :id, ID, required: true
+      argument :status, String, required: false
+      argument :species, String, required: false
+      argument :name, String, required: false
+      argument :character_type, String, required: false
+      argument :gender, String, required: false
+      argument :location_id, Integer, required: false
+      argument :origin_id, Integer, required: false
+    end
+    def edit_character(args)
+      character = Character.find(args[:id])
+      character.update(args.except(:id))
+      character
     end
   end
 end
